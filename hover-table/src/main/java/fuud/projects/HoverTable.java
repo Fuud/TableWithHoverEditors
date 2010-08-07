890d17d204e8d5996758198955c24a6ed1e4a7da
Bobin Fedor <bobin@reksoft.ru>
2010-08-07 18:48:42 +0400

    First test passed.

--- hover-table/src/main/java/fuud/projects/HoverTable.java
+++ hover-table/src/main/java/fuud/projects/HoverTable.java
@@ -1,4 +1,47 @@
 package fuud.projects;
 
-public class HoverTable {
+import org.apache.log4j.Logger;
+
+import javax.swing.*;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
+import java.util.Vector;
+
+public class HoverTable extends JTable {
+    private static final Logger logger = Logger.getLogger(HoverTable.class);
+
+    public HoverTable() {
+    }
+
+    public HoverTable(TableModel dm) {
+        super(dm);
+    }
+
+    public HoverTable(TableModel dm, TableColumnModel cm) {
+        super(dm, cm);
+    }
+
+    public HoverTable(TableModel dm, TableColumnModel cm, ListSelectionModel sm) {
+        super(dm, cm, sm);
+    }
+
+    public HoverTable(int numRows, int numColumns) {
+        super(numRows, numColumns);
+    }
+
+    public HoverTable(Object[][] rowData, Object[] columnNames) {
+        super(rowData, columnNames);
+    }
+
+    public HoverTable(Vector rowData, Vector columnNames) {
+        super(rowData, columnNames);
+    }
+
+    static{
+        logger.trace("class HoverTable loaded");
+    }
+
+    public static void main(String[] arguments) {
+            
+    }
 }
--- hover-table/src/test/java/fuud/projects/HoverTableTest.java
+++ hover-table/src/test/java/fuud/projects/HoverTableTest.java
@@ -1,4 +1,56 @@
 package fuud.projects;
 
+import org.fest.swing.data.TableCell;
+import org.fest.swing.edt.FailOnThreadViolationRepaintManager;
+import org.fest.swing.edt.GuiActionRunner;
+import org.fest.swing.edt.GuiQuery;
+import org.fest.swing.fixture.FrameFixture;
+import org.fest.swing.fixture.JTableFixture;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import static junit.framework.Assert.assertEquals;
+
 public class HoverTableTest {
+    private JTableFixture table;
+    private TableModel model;
+
+    @BeforeClass
+    public static void setUpOnce() {
+        FailOnThreadViolationRepaintManager.install();
+    }
+
+    @Before
+    public void setUp() {
+        JFrame frame = GuiActionRunner.execute(new GuiQuery<JFrame>() {
+            protected JFrame executeInEDT() {
+                model = new DefaultTableModel(2, 2);
+                for (int row = 0; row < 2; row++) {
+                    for (int col = 0; col < 2; col++) {
+                        model.setValueAt(row * col, row, col);
+                    }
+                }
+
+                HoverTable table = new HoverTable(model);
+                final JFrame frame = new JFrame();
+                final JScrollPane scrollPane = new JScrollPane(table);
+                scrollPane.setName("scrollpane");
+                frame.getContentPane().add(scrollPane);
+                return frame;
+            }
+        });
+        FrameFixture window = new FrameFixture(frame);
+        window.show(); // shows the frame to test
+        table = window.table();
+    }
+
+    @Test
+    public void modelWasAccepted() {
+        assertEquals(model.getValueAt(0, 0).toString(), table.valueAt(TableCell.row(0).column(0)));
+    }
 }
